2b2317ba9d56000962b4b88c612e1f821698ecfe
widnat <widnat@gmail.com>
2023-09-20 00:19:50 -0600

    Moved everything over and added necessary packages

---
+++ components/AboutMe.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Image from "next/image";
+import Stack from "react-bootstrap/Stack";
+
+export default function AboutMe() {
+	return (
+		<Stack direction="horizontal" gap={3}>
+			<Image
+				src="/images/HeadShot.png"
+				alt="head shot"
+				width={180}
+				height={190}
+				
+			/>
+			<Stack gap={3}>
+				<div className="bg-light border">First item</div>
+				<div className="bg-light border">Second item</div>
+				<div className="bg-light border">Third item</div>
+			</Stack>
+		</Stack>
+	);
+}
