3dd051eff5860ada7bf1060771df196c348770bd
widnat <widnat@gmail.com>
2023-10-13 21:47:23 -0600

    Changed add player to set the player to sendconid

--- pages/doodler/index.tsx
+++ pages/doodler/index.tsx
@@ -64,7 +64,7 @@ export default function Doodler() {
 	
 			if (message.type === MessageType.AddPlayer) {
 				const addPlayerMessage = JSON.parse(message.value) as AddPlayerMessage;
-				addPlayer(addPlayerMessage, message.recipientConnectionId);
+				addPlayer(addPlayerMessage, message.senderConnectionId);
 			} else if (message.type === MessageType.SubmitAssignmentDoodle) {
 				submitAssignmentDoodle(message);
 			} else if (message.type === MessageType.SubmitFirstGuess) {
