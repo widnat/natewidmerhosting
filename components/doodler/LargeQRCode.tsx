23084eac9c47668dd1114639f5e977561c31e9f2
widnat <widnat@gmail.com>
2023-10-17 00:14:12 -0600

    Fixing error with QRCode

--- components/doodler/LargeQRCode.tsx
+++ components/doodler/LargeQRCode.tsx
@@ -2,9 +2,23 @@ import React from "react";
 import QRCode from "react-qr-code";
 
 type Props = {
-  link: string;
+	link: string;
 };
 
 export default function LargeQRCode({ link }: Props) {
-  return <QRCode size={300} value={link} viewBox={`0 0 300 300`} />;
+	return (
+		<div
+              style={{
+                height: "auto",
+                margin: "0 auto",
+                width: "auto",
+              }}
+            >
+              <QRCode
+                size={300}
+                value={link}
+                viewBox={`0 0 300 300`}
+              />
+            </div>
+	);
 }
